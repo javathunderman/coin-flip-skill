4976afbcb7b058d97826c626b071d8726fc2aa06
javathunderman <javathunderman@nomik.xyz>
2018-02-25 08:52:38 -0500

    reset

--- __init__.py
+++ __init__.py
@@ -16,9 +16,7 @@
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
 from adapt.intent import IntentBuilder
-import feedparser
-import re
-import urllib
+
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
@@ -29,17 +27,31 @@ LOGGER = getLogger(__name__)
 
 class HelloWorldSkill(MycroftSkill):
     def __init__(self):
-        super(NPRFetch, self).__init__(name="NPRFetchSkill")
+        super(HelloWorldSkill, self).__init__(name="HelloWorldSkill")
 
     def initialize(self):
+        thank_you_intent = IntentBuilder("ThankYouIntent"). \
+            require("ThankYouKeyword").build()
+        self.register_intent(thank_you_intent, self.handle_thank_you_intent)
 
-        hello_world_intent = IntentBuilder("FetchNPRIntent"). \
-            require("FetchNPRKeyword").build()
+        how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
+            require("HowAreYouKeyword").build()
+        self.register_intent(how_are_you_intent,
+                             self.handle_how_are_you_intent)
+
+        hello_world_intent = IntentBuilder("HelloWorldIntent"). \
+            require("HelloWorldKeyword").build()
         self.register_intent(hello_world_intent,
                              self.handle_hello_world_intent)
 
+    def handle_thank_you_intent(self, message):
+        self.speak_dialog("welcome")
+
+    def handle_how_are_you_intent(self, message):
+        self.speak_dialog("how.are.you")
+
     def handle_hello_world_intent(self, message):
-        self.speak_dialog("fetching.flash.briefing.from.NPR")
+        self.speak_dialog("hello.world")
 
     def stop(self):
         pass
